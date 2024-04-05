f718c36ab24545caf04aadd939a2eeb8557ccb1f
Swayam Suryavanshi <swayam.suryavanshi@kpit.com>
2024-04-05 11:32:36 +0530

    starter files ready

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-function generateBarcode() {
-    var barcodeData = document.getElementById('barcode-input').value;
-    JsBarcode('#barcode', barcodeData, {
-        format: 'CODE128',
-        width: 1,
-        height: 100,
-        displayValue: true
-    });
-}
+// Barcode Generator
+// TODO: Select the generate barcode button using its id
+// TODO: Add a click event listener to the generate barcode button
+// TODO: Inside the event listener, get the barcode data from the input field
+// TODO: Use the JsBarcode library to generate the barcode
+// TODO: Render the generated barcode in the designated display area
 
-function generateQRCode() {
-    var qrcodeData = document.getElementById('qrcode-input').value;
-    var qrcodeElement = document.getElementById('qrcode');
-    QRCode.toCanvas(qrcodeElement, qrcodeData, function (error) {
-        if (error) console.error(error);
-    });
-}
+// QR Code Generator
+// TODO: Select the generate QR code button using its id
+// TODO: Add a click event listener to the generate QR code button
+// TODO: Inside the event listener, get the QR code data from the input field
+// TODO: Use the qrcode.js library to generate the QR code
+// TODO: Render the generated QR code in the designated display area
